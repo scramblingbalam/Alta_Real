117b92585d6d688d5183fdfcc0fa8dcb2f72b82e
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-02-28 19:18:47 -0500

    NEW walks the directories and encodes correctly also pulls text and id out for each tweet TODO write text to txt file and write id and sent_num to json dic

--- create_corpus_4_doc2vec.py
+++ create_corpus_4_doc2vec.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 train_dir = "Data/semeval2017-task8-dataset"
 train_dic_dir = "traindev"
@@ -15,15 +16,30 @@ rumor_dirs =    {"charlie_hebdo": "charliehebdo",
 				}
 
 ferguson_1 = "498280126254428160"
+ferguson_2 = "498430783699554305"
 
 structutr_path = "/".join([train_dir,train_data_dir,rumor_dirs["ferguson"],ferguson_1,"structure.json"])
-source_path = "/".join([train_dir,train_data_dir,rumor_dirs["ferguson"],ferguson_1,"source-tweet","498280126254428160.json"])
+source_path = "/".join([train_dir,train_data_dir,rumor_dirs["ferguson"],ferguson_2,"source-tweet","498280126254428160.json"])
+ferg_1_path = "/".join([train_dir,train_data_dir,rumor_dirs["ferguson"],ferguson_1])
+ferg_2_path = "/".join([train_dir,train_data_dir,rumor_dirs["ferguson"],ferguson_2])
 
 
+top_path = "/".join([train_dir,train_data_dir])
+top_path = ferg_1_path 
 
-# filedic = {}
-# with open(source_path,"r")as jsonfile:
-# 	filedic= json.load(jsonfile)
+walk = os.walk(top_path)
+for current_dir in walk:
+	last_dir = current_dir[0].split("\\")[-1]
+	if last_dir == "source-tweet" or last_dir == "replies":
+		for json_path in current_dir[-1]:
+			with open(current_dir[0]+"\\"+json_path,"r")as jsonfile:
+				filedic = json.load(jsonfile)
+				print filedic["text"].encode("UTF-8")
+				print filedic["id"]
+				# for k,v in filedic.items():
+				# 	print k
+				# 	print v
+				# 	print "\t",last_dir
 
 # for k,v in filedic.items():
 # 	print k
