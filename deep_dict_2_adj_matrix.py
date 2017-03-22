927279c4a9dbbdda3704728432134404c424441d
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-03-22 15:36:16 -0400

    DEPRICATED found error in new walk function where I didn't change in the recursion from nested_dict.all_keys function it was based on since spyder still had the function in memory it excuted even though I didn't reference fixed it now it work but is still depricated

--- deep_dict_2_adj_matrix.py
+++ deep_dict_2_adj_matrix.py
@@ -7,6 +7,7 @@ Created on Wed Mar 15 16:12:09 2017
 import itertools as it
 import numpy as np
 import pprint
+import nested_dict
 pp = pprint.PrettyPrinter(indent=0)
 
 def flatten(listOfLists):
@@ -14,47 +15,22 @@ def flatten(listOfLists):
     return it.chain.from_iterable(listOfLists)
 
     
-def walk(dic, parent=None, mem_set = None, adj=None):
-    if not isinstance(dic,dict):
-        return adj
+def walk(dic):
+    """
+    TODO:
+        Test against all types 
+        handle python recursion limit
+    """
+    if isinstance(dic,dict):
+        return [(key,subkey) for key,subdic in dic.items() 
+                                   if isinstance(subdic,dict) 
+                                       for subkey in nested_dict.all_keys(subdic)]
     else:
-#        print adj,"DIC"
-        if parent is None and mem_set is None:
-            mem_set = set()
-            for k,v in dic.items():
-                if v:
-                    parent = k
-#                    print k, "K"
-#                    print v, "V"
-                    return walk(v,parent,mem_set)
-                else:
-                    pass
-        elif adj is None:
-#            print "ADJ_None"
-            adj = []
-            for k,v in dic.items():
-                edge = (parent,k)
-                if edge not in mem_set:
-                    adj.append(edge)
-                mem_set.add(edge)
-#                print adj,"ADJ_"
-            for k,v in dic.items():
-#                print adj,"ADJ"
-                if isinstance(v,dict):
-                    adj = walk(v,k,mem_set,adj)
-            return adj
-        else:
-            for k,v in dic.items():
-                edge = (parent,k)
-                if edge not in mem_set:
-                    adj.append(edge)
-                mem_set.add(edge)
-            walk_out =[list(flatten(walk(v,k,mem_set,adj))) for k,v in dic.items()]
-            return walk_out
-            
+        return dic
+
+
               
 def dic_2_adj_mat(Dic):
-    out = walk(Dic)[-1]
     unique_out =[]
     out_set = set()
     for i in out:
@@ -70,9 +46,8 @@ def dic_2_adj_mat(Dic):
         adj_mat[id_dic[i],id_dic[j]]=1
     return adj_mat, id_dic
 
-def dic_2_node_lists(Dic):
-    out = walk(Dic)[0]
-    print out
+def dic_2_node_lists(edge_list):
+    out = edge_list
     unique_out =[]
     out_set = set()
     for i in out:
@@ -80,8 +55,5 @@ def dic_2_node_lists(Dic):
             unique_out.append(i)
             out_set.add(i)   
     id_dic = {ID:num for num,ID in enumerate(unique_out)}
-    walk_out = list(set([( id_dic[parent], id_dic[child] )
-            for parent,child in 
-                zip(out[::2],out[1::2])]))
     return np.array(walk_out), id_dic
 
