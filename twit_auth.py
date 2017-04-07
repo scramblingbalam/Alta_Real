b1fe8e43a020bdc2667a2fbde175f3129ec15230
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-04-06 23:20:26 -0400

    Update twit_auth.py

--- twit_auth.py
+++ twit_auth.py
@@ -2,16 +2,16 @@
 # http://piratefache.ch/twitter-streaming-api-with-tweepy/
 class authentication:
     def __init__(self):
-        self.consumer_key = "hGqgNKnozGGUZB3IyW6Noheky"
-        self.consumer_secret = "MjZAkFlsOzDdikPO5HPoNNjsa6FF7pvx99RIgGWxpSNbGRcjti"
+        self.consumer_key = "############"
+        self.consumer_secret = "#############"
         
         # Go to http://apps.twitter.com and create an app.
         # The consumer key and secret will be generated for you after
         # After the step above, you will be redirected to your app's page.
         # Create an access token under the the "Your access token" section
         
-        self.access_token = "228503532-B6Y7fDmPVP1ppMLG57MJs8jpYfXVMycshIYj4oCc"
-        self.access_token_secret = "OTZ7DtmjMqNuChSK5VDNqoqVDOXtVMQnjUTDAYwXVqAAt"
+        self.access_token = "####################"
+        self.access_token_secret = "########################"
         def getconsumer_key(self):
             return self.consumer_key
         def getconsumer_secret(self):
