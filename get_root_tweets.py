dab57cdfe1e7d08be6e7b92568adaf3f1a219c67
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-04-10 17:52:58 -0400

    ADDED handling for rate_limit calling rate_limit=True and the informas if the waiting for rate limit, chages are in the API call also added a try except that passes if they the exception code == 144 and prints the code if not.

--- get_root_tweets.py
+++ get_root_tweets.py
@@ -10,6 +10,7 @@ import tweepy
 import json
 from bson.son import SON
 from pymongo import MongoClient
+from collections import Counter
 
 pp = pprint.PrettyPrinter(indent=0)
 
@@ -20,7 +21,7 @@ db = client.Alta_Real
     
     # Get access and key from another class
 #    auth = twit_auth.authentication1()
-auth = twit_auth.authentication2()
+auth = twit_auth.authentication3()
 #    auth = twit_auth.authentication3()
 #    auth.
 consumer_key = auth.consumer_key
@@ -34,18 +35,42 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.secure = True
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth)
+api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 trump_tweet_id = 848219027659010051
 Trump_id = 25073877
+error =[]
+trump_ids = list(db.trump_tweets.distinct("id"))
+non_existant_tweet =850184336318103554 
+
+
+
+no_status_error = "[{u'message': u'No status found with that ID.', u'code': 144}]"
 for status_id in db.replies_to_trump.distinct("in_reply_to_status_id"):
-    if status_id:
-        tweet = api.get_status(str(status_id))
-        post = tweet._json
-        post["_id"] = post["id"]
-        trump_tweet_collection = db.trump_tweets
+    if status_id and status_id not in trump_ids:
         try:
-            post_id = trump_tweet_collection.insert_one(post).inserted_id
-        except:
-            pass
+            tweet = api.get_status(str(status_id))
+            post = tweet._json
+            post["_id"] = post["id"]
+            trump_tweet_collection = db.trump_tweets
+            try:
+                post_id = trump_tweet_collection.insert_one(post).inserted_id
+            except:
+                pass
+        except Exception as error:
+            if error[0][0]["code"]==144:
+                pass
+            else:
+                print "ERROR\n",error,"\nERROR"
+
 #print len(db.replies_to_trump.distinct("in_reply_to_status_id"))
+# Code below can be used to print counts of replies
+pipeline = [
+     {"$unwind": "$in_reply_to_status_id"},
+     {"$group": {"_id": "$in_reply_to_status_id", "count": {"$sum": 1}}},
+     {"$sort": SON([("count", -1), ("_id", -1)])}
+ ]
+
+# pp.pprint(list(db.replies_to_trump.aggregate(pipeline)))
+# print Counter([type(i) for i in db.replies_to_trump.distinct("in_reply_to_status_id")])
+
