a16519f0b0e795904aa769f65a1bf5d98649a58b
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-04-10 18:08:38 -0400

    REMOVED KEYS

--- twit_auth.py
+++ twit_auth.py
@@ -1,17 +1,80 @@
 # from:
 # http://piratefache.ch/twitter-streaming-api-with-tweepy/
-class authentication:
+# This is just a place folder all referennces to this code in other scripts 
+# Will be changed to twit_auths but I have kept this in the repo for explication
+class authentication1:
     def __init__(self):
-        self.consumer_key = "hGqgNKnozGGUZB3IyW6Noheky"
-        self.consumer_secret = "MjZAkFlsOzDdikPO5HPoNNjsa6FF7pvx99RIgGWxpSNbGRcjti"
+        self.consumer_key = "######################################"
+        self.consumer_secret = "#######################################"
         
         # Go to http://apps.twitter.com and create an app.
         # The consumer key and secret will be generated for you after
         # After the step above, you will be redirected to your app's page.
         # Create an access token under the the "Your access token" section
         
-        self.access_token = "228503532-B6Y7fDmPVP1ppMLG57MJs8jpYfXVMycshIYj4oCc"
-        self.access_token_secret = "OTZ7DtmjMqNuChSK5VDNqoqVDOXtVMQnjUTDAYwXVqAAt"
+        self.access_token = "###########################################"
+        self.access_token_secret = "#######################################"
+        def getconsumer_key(self):
+            return self.consumer_key
+        def getconsumer_secret(self):
+            return self.consumer_secret
+        def getaccess_token(self):
+            return self.access_token
+        def getaccess_token_secret(self):
+            return self.access_token_secret
+
+class authentication2:
+    def __init__(self):
+        self.consumer_key = "#############################"
+        self.consumer_secret = "####################################"        
+        self.access_token = "##########################################"
+        self.access_token_secret = "################################"
+        def getconsumer_key(self):
+            return self.consumer_key
+        def getconsumer_secret(self):
+            return self.consumer_secret
+        def getaccess_token(self):
+            return self.access_token
+        def getaccess_token_secret(self):
+            return self.access_token_secret
+
+class authentication3:
+    def __init__(self):
+        self.consumer_key = "###################################"
+        self.consumer_secret = "##########################################"        
+        self.access_token = "######################################"
+        self.access_token_secret = "###############################"
+        def getconsumer_key(self):
+            return self.consumer_key
+        def getconsumer_secret(self):
+            return self.consumer_secret
+        def getaccess_token(self):
+            return self.access_token
+        def getaccess_token_secret(self):
+            return self.access_token_secret
+        
+class authentication4:
+    def __init__(self):
+        self.consumer_key = "######################"
+        self.consumer_secret = "##################################"        
+        self.access_token = "####################################"
+        self.access_token_secret = "#######################################"
+        def getconsumer_key(self):
+            return self.consumer_key
+        def getconsumer_secret(self):
+            return self.consumer_secret
+        def getaccess_token(self):
+            return self.access_token
+        def getaccess_token_secret(self):
+            return self.access_token_secret
+        
+        
+class authentication5:
+    def __init__(self):
+        self.consumer_key = "########################################"
+        self.consumer_secret = "#########################################"        
+        self.access_token = "################################################"
+        self.access_token_secret = "#############################################"
         def getconsumer_key(self):
             return self.consumer_key
         def getconsumer_secret(self):
