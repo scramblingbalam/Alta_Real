95b30569c16d9e2e647589187c6d6507609afa34
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-04-11 15:25:28 -0400

    ADDED code which removes documents where either the value of in_reply_to_status_id equals None or where the attempt to dowload the raises and exception

--- get_root_tweets.py
+++ get_root_tweets.py
@@ -4,7 +4,7 @@ Created on Tue Mar 28 15:21:21 2017
 
 @author: scram
 """
-import twit_auth
+import twit_auths as twit_auth
 import pprint
 import tweepy
 import json
@@ -20,10 +20,11 @@ db = client.Alta_Real
 #db = client['test-database']
     
     # Get access and key from another class
-#    auth = twit_auth.authentication1()
-auth = twit_auth.authentication3()
-#    auth = twit_auth.authentication3()
-#    auth.
+#auth = twit_auth.authentication1()
+#auth = twit_auth.authentication3()
+auth = twit_auth.authentication4()
+#auth = twit_auth.authentication5()
+
 consumer_key = auth.consumer_key
 consumer_secret = auth.consumer_secret
 
@@ -43,11 +44,13 @@ error =[]
 trump_ids = list(db.trump_tweets.distinct("id"))
 non_existant_tweet =850184336318103554 
 
-
+print db.replies_to_trump.distinct("in_reply_to_status_id")
 
 no_status_error = "[{u'message': u'No status found with that ID.', u'code': 144}]"
 for status_id in db.replies_to_trump.distinct("in_reply_to_status_id"):
-    if status_id and status_id not in trump_ids:
+    if not status_id:
+        print db.replies_to_trump.remove({"in_reply_to_status_id":status_id})  
+    elif status_id not in trump_ids:
         try:
             tweet = api.get_status(str(status_id))
             post = tweet._json
@@ -58,10 +61,13 @@ for status_id in db.replies_to_trump.distinct("in_reply_to_status_id"):
             except:
                 pass
         except Exception as error:
-            if error[0][0]["code"]==144:
-                pass
-            else:
-                print "ERROR\n",error,"\nERROR"
+            print "\tERROR\n",error,"\n",status_id,"\n\tERROR"
+            removed_ids =[i["_id"] for i in db.replies_to_trump.find({"in_reply_to_status_id":status_id})]
+            print db.replies_to_trump.remove({"in_reply_to_status_id":status_id}) 
+            for rm_id in removed_ids:
+                print "Documnet removed",rm_id
+            
+            
 
 #print len(db.replies_to_trump.distinct("in_reply_to_status_id"))
 # Code below can be used to print counts of replies
