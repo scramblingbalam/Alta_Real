c504b588e54081e8a1810edc0ad89d0e76bf3760
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-04-16 17:40:00 -0400

    ADDED modularized the main functanality into a unction get_root_drop_branches that takes child_collection parent_collection and db as inputs

--- get_root_tweets.py
+++ get_root_tweets.py
@@ -14,69 +14,62 @@ from collections import Counter
 
 pp = pprint.PrettyPrinter(indent=0)
 
-client = MongoClient()
-client = MongoClient('localhost', 27017)
-db = client.Alta_Real
-#db = client['test-database']
-    
-# Get access and key from another class
-#auth = twit_auth.authentication1()
-#auth = twit_auth.authentication3()
-auth = twit_auth.authentication4()
-#auth = twit_auth.authentication5()
 
-consumer_key = auth.consumer_key
-consumer_secret = auth.consumer_secret
-
-access_token = auth.access_token
-access_token_secret = auth.access_token_secret
-
-# Authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.secure = True
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
-trump_tweet_id = 848219027659010051
-Trump_id = 25073877
-error =[]
-trump_ids = list(db.trump_tweets.distinct("id"))
-non_existant_tweet =850184336318103554 
-
-print db.replies_to_trump.distinct("in_reply_to_status_id")
-
-no_status_error = "[{u'message': u'No status found with that ID.', u'code': 144}]"
-for status_id in db.replies_to_trump.distinct("in_reply_to_status_id"):
-    if not status_id:
-        print db.replies_to_trump.remove({"in_reply_to_status_id":status_id})  
-    elif status_id not in trump_ids:
-        try:
-            tweet = api.get_status(str(status_id))
-            post = tweet._json
-            post["_id"] = post["id"]
-            trump_tweet_collection = db.trump_tweets
+def get_root_drop_branches(child_collection,parent_collection,db):
+    trump_ids = list(db[parent_collection].distinct("id"))
+    #no_status_error = "[{u'message': u'No status found with that ID.', u'code': 144}]"
+    for status_id in db[child_collection].distinct("in_reply_to_status_id"):
+        if not status_id:
+            print dict(db["child_collection"].delete_many({"in_reply_to_status_id":status_id})),"DELETE_MANY"
+#            for i in db[child_collection].remove({"in_reply_to_status_id":status_id}):
+#                print "Documnet removed",i
+        elif status_id not in trump_ids:
             try:
-                post_id = trump_tweet_collection.insert_one(post).inserted_id
-            except:
-                pass
-        except Exception as error:
-            print "\tERROR\n",error,"\n",status_id,"\n\tERROR"
-            removed_ids =[i["_id"] for i in db.replies_to_trump.find({"in_reply_to_status_id":status_id})]
-            print db.replies_to_trump.remove({"in_reply_to_status_id":status_id}) 
-            for rm_id in removed_ids:
-                print "Documnet removed",rm_id
+                tweet = api.get_status(str(status_id))
+                post = tweet._json
+                post["_id"] = post["id"]
+                trump_tweet_collection = db[parent_collection]
+                try:
+                    post_id = trump_tweet_collection.insert_one(post).inserted_id
+                except:
+                    pass
+            except Exception as error:
+                print "\tERROR\n",error,"\n",status_id,"\n\tERROR"
+    #            removed_ids =[i["_id"] for i in db["child_collection"].find({"in_reply_to_status_id":status_id})]
+                print db["child_collection"].delete_many({"in_reply_to_status_id":status_id})
+#                for rm_id in db["child_collection"].delete_many({"in_reply_to_status_id":status_id}):
+#                    print "Documnet removed",rm_id
             
             
 
-#print len(db.replies_to_trump.distinct("in_reply_to_status_id"))
-# Code below can be used to print counts of replies
-pipeline = [
-     {"$unwind": "$in_reply_to_status_id"},
-     {"$group": {"_id": "$in_reply_to_status_id", "count": {"$sum": 1}}},
-     {"$sort": SON([("count", -1), ("_id", -1)])}
- ]
-
-# pp.pprint(list(db.replies_to_trump.aggregate(pipeline)))
-# print Counter([type(i) for i in db.replies_to_trump.distinct("in_reply_to_status_id")])
-
+if __name__ == "__main__":
+    client = MongoClient()
+    client = MongoClient('localhost', 27017)
+    DB = client.Alta_Real
+    DB = client['test-database']
+        
+    # Get access and key from another class
+    #auth = twit_auth.authentication1()
+    #auth = twit_auth.authentication3()
+    auth = twit_auth.authentication4()
+    #auth = twit_auth.authentication5()
+    
+    consumer_key = auth.consumer_key
+    consumer_secret = auth.consumer_secret
+    
+    access_token = auth.access_token
+    access_token_secret = auth.access_token_secret
+    
+    # Authentication
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.secure = True
+    auth.set_access_token(access_token, access_token_secret)
+    
+    api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        
+#    parent_coll_name = "trump_tweets"
+#    child_coll_name = "replies_to_trump"
+    
+    parent_coll_name = "test_parents"
+    child_coll_name = "trump_tweets"
+    get_root_drop_branches(child_coll_name,parent_coll_name,DB)
