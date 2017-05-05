7cc575b5c49c92de5a8f87f9362b0bd0eae20ecf
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-05-04 21:55:26 -0400

    ADDED the remove_old_threads function whihc removes old threads and all their tweets from trump_tweets and replies_to_trump this closes #59

--- Mongo_functions.py
+++ Mongo_functions.py
@@ -82,9 +82,6 @@ def edge_list(collections_list,DB):
     #                                    })
     #                            edge_coll.delete_one({"_id":edge[1]})                
                         elif edge[0] not in root_ids:
-    #                        if 851596235949658112 in tweet.values():
-    #                            print "\tELIF_1"
-    #                            print "\t",edge[0],edge[1]
                             if db[parent_coll].find({'_id':edge[0]}):
                                 post = {"_id": edge[0],
                                     "edge_list": [
@@ -99,11 +96,7 @@ def edge_list(collections_list,DB):
                             else:
                                 db[child_coll].delete_one({'_id':tweet['id']})
                                 
-                        elif edge[1] not in child_ids:  
-    #                        if 851596235949658112 in tweet.values():
-    #                            print "\tELIF_2"
-    #                            print "\t",edge[0],edge[1]
-    #                        if db[parent_coll].find({'_id':edge[0]}):
+                        elif edge[1] not in child_ids:
                             edge_coll.update_one(
                                     {"_id":edge[0]},
                                     {"$push": {"edge_list":
@@ -113,9 +106,7 @@ def edge_list(collections_list,DB):
                                     })
                             child_ids.add(edge[1])
                             parent_ids.add(edge[0])
-                            print edge[0],"^--",edge[1] 
-    #                        else:
-#                            db[child_coll].delete_one({'_id':tweet['id']})
+                            print edge[0],"^--",edge[1]
             print "Finished first collection"
         else:
             # Decide if you want to do anything to with this to support different
@@ -149,10 +140,10 @@ def remove_old_threads(DB,id_date):
             del_roots +=1
             id_list = list(id_set - set([thread['_id']]))
             del_replies += len(id_list)
-#            DB.trump_tweets.delete_one({'_id':thread['_id']})
-#            del_dic ={'_id':{'$is':list(id_set)}}
-#            DB.replies_to_trump.delete_many(del_dic)
-#            DB.edge_list.delete_one({'_id':thread['_id']})
+            DB.trump_tweets.delete_one({'_id':thread['_id']})
+            del_dic ={'_id':{'$in':list(id_set)}}
+            DB.replies_to_trump.delete_many(del_dic)
+            DB.edge_list.delete_one({'_id':thread['_id']})
             del_threads +=1
         else:
             for edge in thread['edge_list']:
@@ -164,10 +155,10 @@ def remove_old_threads(DB,id_date):
                 del_roots +=1
                 id_list = list(id_set - set([thread['_id']]))
                 del_replies += len(id_list)
-#                DB.trump_tweets.delete_one({'_id':thread['_id']})
-#                del_dic ={'_id':{'$is':list(id_set)}}
-#                DB.replies_to_trump.delete_many(del_dic)
-#                DB.edge_list.delete_one({'_id':thread['_id']})
+                DB.trump_tweets.delete_one({'_id':thread['_id']})
+                del_dic ={'_id':{'$in':list(id_set)}}
+                DB.replies_to_trump.delete_many(del_dic)
+                DB.edge_list.delete_one({'_id':thread['_id']})
                 del_threads +=1
         
     changes = [del_replies,del_roots,del_threads]
@@ -200,15 +191,15 @@ if __name__ == "__main__":
 #    print list(big_thread)
     biggest_id =  list(big_thread)[0]["_id"]
     print biggest_id
-#    DG=nx.DiGraph()
-#    DG.add_edges_from(id2edgelist(biggest_id,edge_collection))
-#    nx.draw_random(DG, with_labels=False)
-#    nx.draw_random(DG, with_labels=True)
+    DG=nx.DiGraph()
+    DG.add_edges_from(id2edgelist(biggest_id,edge_collection))
+    nx.draw_random(DG, with_labels=False)
+    nx.draw_random(DG, with_labels=True)
 #    """
     old_id = 856425255862235136
 #    oldest_time = list(db.replies_to_trump.find({"_id":old_id}))[0]["created_at"]
 #    oldest_id = list(db.replies_to_trump.find({"_id":old_id}))[0]["_id"]
-#    remove_old_threads(db,old_id)
+    remove_old_threads(db,old_id)
 #    remove_old_threads(db,oldest_time)
     
     
