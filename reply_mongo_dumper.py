1db9c55179ccabb6b3a3f08d3bf15157fedbc22a
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-05-11 16:35:29 -0400

    My attempts to work this script into one which would get all levels of replies and fullfil #61 were not productive I have rebased the code from the local test version tweet_mongo_dumper.py and added some features that on how to handle time outs and start the script over using the smallest/oldest status_id this srcipt now functions only to download the first order replies to an account.

--- reply_mongo_dumper.py
+++ reply_mongo_dumper.py
@@ -11,151 +11,98 @@ Created on Tue Mar 28 15:18:19 2017
 import tweepy #https://github.com/tweepy/tweepy
 import twit_auths as twit_auth
 from pymongo import MongoClient
-from collections import Counter
+
+#Twitter API credentials fron twit_auth
+# auth = twit_auth.authentication1()
+#auth = twit_auth.authentication2()
+# auth = twit_auth.authentication3()
+# auth = twit_auth.authentication4()
+auth = twit_auth.authentication5()
 
 
-def get_all_replies(screen_name_id_dict,db):
+# this code needs to change for Python 3 to use the function
+consumer_key = auth.consumer_key
+consumer_secret = auth.consumer_secret
+access_token = auth.access_token
+access_token_secret = auth.access_token_secret
+
+
+#MongoDB credentials and collections
+# DBname = 'test-database'
+DBname = 'Alta_Real_New'
+DBhost = 'localhost'
+DBport = 27017
+
+# initiate Mongo Client
+client = MongoClient()
+client = MongoClient(DBhost, DBport)
+db = client[DBname]
+
+
+
+def get_all_replies(screen_name_list,oldest =None):
 	#Twitter only allows access to a users most recent 3240 tweets with this method
-    if isinstance(screen_name_id_dict,dict):
-        screen_name_list = screen_name_id_dict.keys()
-        tweet_id_list = screen_name_id_dict.values()[0]
-    elif isinstance(screen_name_id_dict,list):
-        screen_name_list = screen_name_id_dict
-        tweet_id_list = None
+	
 	#authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth,wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-	
+    api = tweepy.API(auth,wait_on_rate_limit=True, 
+                 wait_on_rate_limit_notify=True,
+                 retry_count = 10,
+                 retry_errors=10054,
+                 retry_delay=5) 
     #initialize a list to hold all the tweepy Tweets
+    alltweets = []	
 	
     #make initial request for most recent tweets (200 is the maximum allowed count)
 #    new_tweets = api.search(" to:".join(screen_name_list),count=100)
-    new_tweets = api.search(" to:".join(screen_name_list),count=100)
+    if oldest:
+        new_tweets = api.search(" to:".join(screen_name_list),count=100,max_id=oldest)
+    else:
+        new_tweets = api.search(" to:".join(screen_name_list),count=100)
     #save most recent tweets
     i = len(new_tweets)
-    posted = 0
-    errors = []
-    #save the id of the oldest tweet less one
-    collection = db.replies_to_trump
+        # ins
     for tweet in new_tweets:
         post = tweet._json
         post["_id"] = post["id"]
-        if post['in_reply_to_status_id']:
-            if tweet_id_list:
-                if post['in_reply_to_status_id'] in tweet_id_list:
-                    try:
-                        post_id = collection.insert_one(post).inserted_id
-                        posted +=1
-                    except Exception as err1:
-                        errors.append(err1.code)
-                        pass
-            else:
-                if post['in_reply_to_screen_name'] in screen_name_list:
-                    try:
-                        post_id = collection.insert_one(post).inserted_id
-                        posted +=1
-                    except Exception as err2:
-                        errors.append(err2.code)
-                        pass
-    if new_tweets:
-        oldest = new_tweets[-1].id - 1
-        print "...%s tweets downloaded so far" % (i)
-        print "\t%s/%s"%(posted,i),"posted/downloaded"
-        print "Post Errors"
-        for k,v in Counter(errors).items():
-            print "\t",k,v
-    else:
-        print "No more tweets for users:",", ".join(screen_name_list)
-        
-        
+        trump_tweet_collection = db.replies_to_trump
+        try:
+            post_id = trump_tweet_collection.insert_one(post).inserted_id
+        except:
+            pass
+    #save the id of the oldest tweet less one
+    oldest = new_tweets[-1].id - 1
+#    oldest = 850452920243961855
+
     #keep grabbing tweets until there are no tweets left to grab
     while len(new_tweets) > 0:
-#        print "getting tweets before %s" % (oldest)
+        print "getting tweets before %s" % (oldest)
         #all subsiquent requests use the max_id param to prevent duplicates
         new_tweets = api.search(" to:".join(screen_name_list),count=100,max_id=oldest)
         i += len(new_tweets)
         #update the id of the oldest tweet less one
-
+        oldest = new_tweets[-1].id - 1
+        print "...%s tweets downloaded so far" % (i)
     # ins
         for tweet in new_tweets:
             post = tweet._json
             post["_id"] = post["id"]
-            if post['in_reply_to_status_id']:
-                if tweet_id_list:
-                    if post['in_reply_to_status_id'] in tweet_id_list:
-                        try:
-                            post_id = collection.insert_one(post).inserted_id
-                            posted +=1
-                        except Exception as err3:
-                            errors.append(err3.code)
-                            pass
-                else:
-                    if post['in_reply_to_screen_name'] in screen_name_list:
-                        try:
-                            post_id = collection.insert_one(post).inserted_id
-                            posted +=1
-                        except Exception as err4:
-                            errors.append(err4.code)
-                            pass
-                        
-        if new_tweets:
-            oldest = new_tweets[-1].id - 1
-            print "...%s tweets downloaded so far" % (i)
-            print "\t%s/%s"%(posted,i),"posted/downloaded"
-            if errors:
-                print "Post Errors"
-                for k,v in Counter(errors).items():
-                    print "\t",k,v
-        else:
-            print "No more tweets for users:",", ".join(screen_name_list)
-
+            trump_tweet_collection = db.replies_to_trump
+            try:
+                post_id = trump_tweet_collection.insert_one(post).inserted_id
+            except:
+                pass
 
 if __name__ == '__main__':
+    name_list = ['realDonaldTrump']
+    pipeline_oldest =[{"$group":
+                {"_id":"$in_reply_to_screen_name",
+                "oldest":{"$min":"$id"},
+                "newest":{"$max":"$id"},
+                "count": {"$sum":1}
+                }    
+            }]
+    oldest_id =list(DB.trump_tweets.aggregate(pipeline_oldest))[0]["oldest"]
     #pass in the username of the account you want to download
-    #Twitter API credentials fron twit_auth
-#    auth = twit_auth.authentication1()
-#    auth = twit_auth.authentication2()
-#    auth = twit_auth.authentication3()
-    auth = twit_auth.authentication4()
-#    auth = twit_auth.authentication5()
-
-    # this code needs to change for Python 3 to use the function
-    consumer_key = auth.consumer_key
-    consumer_secret = auth.consumer_secret
-    access_token = auth.access_token
-    access_token_secret = auth.access_token_secret
-    
-    
-    #MongoDB credentials and collections
-    DBname = 'test-tree-new'
-    #DBname = 'Alta_Real'
-    DBname = 'test-tree'
-    DBhost = 'localhost'
-    DBport = 27017
-    
-    # initiate Mongo Client
-    client = MongoClient()
-    client = MongoClient(DBhost, DBport)
-    DB = client[DBname]
-#    pipeline = [
-#      {"$group": {"_id": { "screen_name": "$user.screen_name"}, 
-#        "uniqueIds": { "$addToSet": "$_id" },
-#      }}, 
-#    ]
-    pipeline = [
-      {"$group": {"_id": "$user.screen_name", 
-        "uniqueIds": { "$addToSet": "$_id" },
-      }}, 
-    ]
-    root_name = "drchuck"
-#    root_name = "realDonaldTrump"
-    screen_name_ID_dic = []
-    screen_name_ID_dic = list(DB.replies_to_trump.aggregate(pipeline))
-    screen_name_ID_dic = []
-    screen_name_ID_dic = list(DB.trump_tweets.aggregate(pipeline))
-    if not screen_name_ID_dic:
-        name_id_dic = [root_name]
-    else:
-        name_id_dic = {i["_id"]:i["uniqueIds"] for i in screen_name_ID_dic}
-    get_all_replies(name_id_dic,DB)
+    get_all_replies(name_list,oldest_id)
