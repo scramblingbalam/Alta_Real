33a7966da54ec8f7ee2e390eb75c5693d6743bd3
Colin A. Colin A. Drayton <cdrayton@umich.edu>
2017-05-15 18:04:39 -0400

    FIRST COMMIT this script creates the word2vec documents from the mongoDB and is used to run the t-CRF on the Trump tweets in #44

---
+++ create_corpus_4_doc2vec_4_mongo_PY3.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Thu Mar 02 16:32:18 2017
+
+@author: Colin Dryaton cdrayton@umich.edu
+"""
+
+
+import json
+#import cPickle as pickle
+import twit_token
+import unicodedata as uniD
+import nltk
+import re
+from pymongo import MongoClient
+
+#MongoDB credentials and collections
+#DBname = 'test-tree'
+#DBname = 'test_recurse'
+DBname = 'Alta_Real_New'
+DBhost = 'localhost'
+DBport = 27017
+    
+# initiate Mongo Client
+client = MongoClient()
+client = MongoClient(DBhost, DBport)
+db = client[DBname]
+# this code needs to change for Python 3 to use the function
+reply_db = db.replies_to_trump
+root_db = db.trump_tweets
+
+
+#top_path = ferg_1_path 
+
+zub_id_text_dic = {}
+twit_id_text_dic = {}
+
+zub_text_list = []
+twit_text_list = []
+
+id_list = []
+
+
+
+source_id = ""
+reply_id = ""
+
+for tweet in list(root_db.find())+list(reply_db.find()):
+    text =  tweet["text"].lower()
+                
+    zub_text = " ".join(nltk.word_tokenize(re.sub(r'([^\s\w]|_)+', '',text)))
+    zub_id_text_dic[tweet["id"]] = zub_text
+    zub_text_list.append(zub_text)
+                
+    text_in = text.replace("\n","N3WL1N3")#+'\r\n'
+
+    twit_text = " ".join(twit_token.ize(text_in))
+    twit_id_text_dic[tweet["id"]] = twit_text
+    twit_text_list.append(twit_text)
+
+    id_list.append(tweet["id"])
+                
+# I save all the containers I use to create teh doc2vec training file 
+# I do this to make sure that debugging doc2vec will be easy and 
+# I'll have all the data I need to ask any question I want to
+
+for z_tweet,t_tweet in zip(zub_text_list,twit_text_list[:10]):
+    print(z_tweet)
+    print(t_tweet,'\n')      
+              
+doc2vec_dir ="Data/doc2vec/trump"
+with open(doc2vec_dir+"twit_id_text_dic.json","w") as picfile:
+    json.dump(twit_id_text_dic,picfile)
+
+with open(doc2vec_dir+"twit_text_list.json","w") as picfile:
+    json.dump(twit_text_list,picfile)    
+    
+with open(doc2vec_dir+"twit_doc2vec_train_corpus.txt","wb")as corpusfile:
+    corpusfile.writelines([txt.encode("utf8")+"\r\n".encode("utf8") for txt in twit_text_list])
+
+
+
+with open(doc2vec_dir+"zub_id_text_dic.json","w") as picfile:
+    json.dump(zub_id_text_dic,picfile)
+
+with open(doc2vec_dir+"zub_text_list.json","w") as picfile:
+    json.dump(zub_text_list,picfile)    
+
+with open(doc2vec_dir+"zub_doc2vec_train_corpus.txt","wb")as corpusfile:
+    corpusfile.writelines([txt.encode("utf8")+"\r\n".encode("utf8") for txt in zub_text_list])
+
+
+
+with open(doc2vec_dir+"id_list.json","w") as picfile:
+    json.dump(id_list,picfile)     
+
+
