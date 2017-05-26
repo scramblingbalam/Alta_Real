8602dc72301fcc70f209094d0a9f53ef8600f9c0
Colin A. Drayton <cdrayton@umich.edu>
2017-05-26 12:38:04 -0400

    ADDED function  that will walk the original data and add it to mongo, also function  is py2 and py3 compatible

--- feature_creation_mongo.py
+++ feature_creation_mongo.py
@@ -38,20 +38,17 @@ token_type = "twit"
 #embed_type = "doc2vec"
 
 
-train_dir = "Data\\semeval2017-task8-dataset"
+
 train_dic_dir = "traindev"
 
-train_data_dir ="rumoureval-data"
 
-
-top_path = "\\".join([train_dir,train_data_dir])
 
 
 id_text_dic = {}
 text_list = []
 id_list = []
 
-walk = os.walk(top_path)
+
 
 
 POS_dir ="Data\\twitIE_pos\\"
@@ -62,7 +59,8 @@ ids_around_IDless_tweets=[136,139,1085,1087]
 
 #non_english_event =[]
 
-pos_file_path =POS_dir+"corpus_twitIE_POS"
+
+pos_file_path = POS_dir+token_type+"_semeval2017"+"_twitIE_POS"
 id_pos_dic, index_pos_dic = feature.pos_extract(pos_file_path)
 
 
@@ -100,7 +98,6 @@ with open(doc2vec_dir+"id_list.json","r") as picfile:
 D2V_id_text_dic ={}
 with open(doc2vec_dir+token_type+"_"+"id_text_dic.json", "r")as d2vtextfile:
     D2V_id_text_dic = json.load(d2vtextfile)
-
 
 
 #print "KeyError: u'552790281276628992'"
@@ -262,7 +259,40 @@ thread_list=[]
 #        else:
 #            event_ID_dic[event] = [thread_id_list]
 #        thread_dic = {}
-
+def files_to_mongo(root_path,db,collection):
+    """ Moves tweets from their file structure to mongo
+    """
+    walk = os.walk(root_path)
+    for current_dir in walk:
+        if 'structure.json' in current_dir[-1]:
+            event = current_dir[0].split("\\")[-2]
+        last_dir = current_dir[0].split("\\")[-1]
+        if last_dir == "replies":
+            thread_id_list = []
+            feature_vector =[]
+            json_list =[]
+            rep_path =current_dir[0]
+            root_path = rep_path.split("\\")
+            with open("\\".join(root_path[:-1])+"\\"+'structure.json',"r")as jsonfile:
+                structure = json.load(jsonfile)
+    
+            source_path = root_path
+            source_path[-1]="source-tweet"
+            source_path = "\\".join(source_path)
+            root_id =os.listdir(source_path)[0]
+            with open(source_path+"\\"+root_id,"r")as jsonfile:
+                json_list.append(json.load(jsonfile))
+            for json_path in current_dir[-1]:
+                with open(current_dir[0]+"\\"+json_path,"r")as jsonfile:
+                    json_list.append(json.load(jsonfile))
+            print(list(map(lambda x: x.get("id"),json_list)))
+            print(event)
+            edge_list = nested_dict
+            print(structure)
+            structure = nested_dict.subset_by_key(structure, thread_dic.keys())
+            size = len(nested_dict.all_keys(structure))
+            edge_list = nested_dict.to_edge_list(structure)
+            print("\n--------------------")
 
 
 
@@ -309,6 +339,8 @@ def process_tweet(tweet):
 #    thread_dic[ID] = feature_vector
     return ID, feature_vector
 
+
+#### RUN FUNCTIONS TO CREATE FEATURES 
 if __name__ == '__main__':
     #MongoDB credentials and collections
 #    DBname = 'test-tree'
@@ -321,102 +353,72 @@ if __name__ == '__main__':
     client = MongoClient()
     client = MongoClient(DBhost, DBport)
     DB = client[DBname]
-    for current_dir in walk:
-        adj_mat = np.array([])
-    #    print "\nEVENT",event
-    #    print "CURRENT_DIR",current_dir
-        if 'structure.json' in current_dir[-1]:
-    #        print current_dir[0].split("\\")[-2]
-            event = current_dir[0].split("\\")[-2]
-    #        print "##############\n",event
-    ##        if adj_mat.any():
-    ##            G = nx.from_numpy_matrix(np.array(adj_mat)) 
-    ##            nx.draw(G, with_labels=True)
-        last_dir = current_dir[0].split("\\")[-1]
-        if last_dir == "replies":
-            thread_id_list = []
-            feature_vector =[]
-            json_list =[]
-            rep_path =current_dir[0]
-    #        print "REPLIES_DIR",rep_path ,"\n"
-            root_path = rep_path.split("\\")
-    #        print root_path
-            with open("\\".join(root_path[:-1])+"\\"+'structure.json',"r")as jsonfile:
-    #            print structure
-                structure = json.load(jsonfile)
+    # collection where SemEval data is stored
+    collection_train = "semeval2017_task8"
     
-            source_path = root_path
-            source_path[-1]="source-tweet"
-            source_path = "\\".join(source_path)
-    #        print list(os.listdir(source_path))
-            root_id =os.listdir(source_path)[0]
-    #        if root_id.split(".")[0] in id_target_dic.keys():
-            with open(source_path+"\\"+root_id,"r")as jsonfile:
-                json_list.append(json.load(jsonfile))
-            for json_path in current_dir[-1]:
-                with open(current_dir[0]+"\\"+json_path,"r")as jsonfile:
-                    json_list.append(json.load(jsonfile))
-    #        print "\n", root_id
+    if not DB[collection_train].find_one():    
+        train_data_dir ="rumoureval-data"
+        train_dir = "Data\\semeval2017-task8-dataset"
+        top_path = "\\".join([train_dir,train_data_dir])
+        files_to_mongo(top_path,DB,collection)
     ######## Process each tweet to create a feature vector
-            for tweet in json_list:
-                twt_ID, feat_vector = process_tweet(tweet)
-                thread_id_list.append(twt_ID)
-                thread_dic[twt_ID] = feat_vector
-            structure = nested_dict.subset_by_key(structure, thread_dic.keys())
-            size = len(nested_dict.all_keys(structure))
-            edge_list = nested_dict.to_edge_list(structure)
-            if 60>size>graph_size:
-                graph_2_vis = edge_list
-                graph_size = size
-                graph_event = event
-                graph_root_id =root_id
-                
-            id_dic = feature.id_index_dic(edge_list)
-            id_order = [i[0] for i in sorted(id_dic.items(),key=lambda x:(x[1],x[0]))]
-            # create an array for each thread an append to the event_target_dic        
-            if event != "germanwings-crash":
-                thread_target_vector = [np.array(map(translatelabel, 
-                                                    [id_target_dic[i] 
-                                                                for i in id_order]))]
-    
-                if event in event_model_dic:
-                    event_target_dic[event] += thread_target_vector
-                else:
-                    event_target_dic[event] = thread_target_vector
-       
-    #        pp.pprint(structure)
-            edge_vector = np.array([np.array([id_dic[Id] for Id in edge]) 
-                                                            for edge in edge_list])
-    
-            n_feats = np.array([thread_dic[i] for i in id_order])
-            X_train = [np.array([n_feats,edge_vector])]
-            if event in event_model_dic:
-                event_model_dic[event] += X_train
-            else:
-                event_model_dic[event] = X_train
-            
-            if event in event_ID_dic:
-                event_ID_dic[event] += [thread_id_list]
-            else:
-                event_ID_dic[event] = [thread_id_list]
-            thread_dic = {}
+#            for tweet in json_list:
+#                twt_ID, feat_vector = process_tweet(tweet)
+#                thread_id_list.append(twt_ID)
+#                thread_dic[twt_ID] = feat_vector
 
-    event_model_dic = ["_".join([token_type,embed_type,dims]),event_model_dic]
-    
-    with open("event_model_dic","w")as modelfile:
-        pickle.dump(event_model_dic,modelfile)
-    
-    with open("event_target_dic","w")as modelfile:
-        pickle.dump(event_target_dic,modelfile)
-    
-    with open("event_ID_dic","w")as modelfile:
-        pickle.dump(event_ID_dic,modelfile)
-    
-    print( event_ID_dic.keys())
-    #print graph_size
-    #print graph_event, graph_root_id
-    print(graph_2_vis) 
-    #DG=nx.DiGraph()
+#            if 60>size>graph_size:
+#                graph_2_vis = edge_list
+#                graph_size = size
+#                graph_event = event
+#                graph_root_id =root_id
+#                
+#            id_dic = feature.id_index_dic(edge_list)
+#            id_order = [i[0] for i in sorted(id_dic.items(),key=lambda x:(x[1],x[0]))]
+#            # create an array for each thread an append to the event_target_dic        
+#            if event != "germanwings-crash":
+#                thread_target_vector = [np.array(map(translatelabel, 
+#                                                    [id_target_dic[i] 
+#                                                                for i in id_order]))]
+#    
+#                if event in event_model_dic:
+#                    event_target_dic[event] += thread_target_vector
+#                else:
+#                    event_target_dic[event] = thread_target_vector
+#       
+#    #        pp.pprint(structure)
+#            edge_vector = np.array([np.array([id_dic[Id] for Id in edge]) 
+#                                                            for edge in edge_list])
+#    
+#            n_feats = np.array([thread_dic[i] for i in id_order])
+#            X_train = [np.array([n_feats,edge_vector])]
+#            if event in event_model_dic:
+#                event_model_dic[event] += X_train
+#            else:
+#                event_model_dic[event] = X_train
+#            
+#            if event in event_ID_dic:
+#                event_ID_dic[event] += [thread_id_list]
+#            else:
+#                event_ID_dic[event] = [thread_id_list]
+#            thread_dic = {}
+#
+#    event_model_dic = ["_".join([token_type,embed_type,dims]),event_model_dic]
+#    
+#    with open("event_model_dic","wb")as modelfile:
+#        pickle.dump(event_model_dic,modelfile)
+#    
+#    with open("event_target_dic","wb")as modelfile:
+#        pickle.dump(event_target_dic,modelfile)
+#    
+#    with open("event_ID_dic","wb")as modelfile:
+#        pickle.dump(event_ID_dic,modelfile)
+#    
+#    print( event_ID_dic.keys())
+#    #print graph_size
+#    #print graph_event, graph_root_id
+#    print(graph_2_vis) 
+#    #DG=nx.DiGraph()
     #DG.add_edges_from(graph_2_vis)
     #nx.draw_random(DG, with_labels=False)
     
